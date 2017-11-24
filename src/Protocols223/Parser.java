32a54b4972b5d0270c17690d3c46fc26f2978057
rummolprod999 <rummolprod999@gmail.com>
2017-11-24 17:25:03 +0300

    upd

--- src/Protocols223/Parser.java
+++ src/Protocols223/Parser.java
@@ -1,9 +1,17 @@
 package Protocols223;
 
+import org.apache.commons.net.ftp.FTP;
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
+
+import java.io.File;
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import static java.lang.System.out;
+import static java.lang.Thread.sleep;
 
 public class Parser implements IParser {
 
@@ -13,4 +21,38 @@ public class Parser implements IParser {
     }
 
     public String[] protocols223Dir = {"purchaseProtocol", "purchaseProtocolIP", "purchaseProtocolOSZ", "purchaseProtocolPA_AE", "purchaseProtocolPA_OA", "purchaseProtocolPAAE", "purchaseProtocolPAAE94", "purchaseProtocolPAEP", "purchaseProtocolPAOA", "purchaseProtocolRKZ", "purchaseProtocolRZ1AE", "purchaseProtocolRZ2AE", "purchaseProtocolRZ_AE", "purchaseProtocolRZ_OA", "purchaseProtocolRZ_OK", "purchaseProtocolRZAE", "purchaseProtocolRZOA", "purchaseProtocolRZOK", "purchaseProtocolVK", "purchaseProtocolZK"};
+
+    ArrayList<FTPFile> GetListFtp223(String pathParse){
+        ArrayList<FTPFile> arr = new ArrayList<>();
+        int count = 1;
+        while (true){
+            try{
+                arr = FtpLst(pathParse, "fz223free", "fz223free");
+            }
+            catch (Exception e){
+                if (count > 3)
+                {
+                    Log.Logger(String.format("Не смогли найти директорию после попытки %d", count), pathParse, e);
+                    break;
+                }
+                count++;
+                try {
+                    sleep(5000);
+                } catch (InterruptedException ignored) {
+
+                }
+            }
+        }
+
+        return arr;
+    }
+
+    private ArrayList<FTPFile> FtpLst(String pathParse, String login, String pass) throws IOException {
+        FTPClient ftpClient = new FTPClient();
+        ftpClient.connect("ftp.zakupki.gov.ru");
+        ftpClient.login(login, pass);
+        ftpClient.enterLocalPassiveMode();
+        ftpClient.changeWorkingDirectory(pathParse);
+        return new ArrayList<>(Arrays.asList(ftpClient.listDirectories()));
+    }
 }
--- src/Protocols223/ParserProtocols223.java
+++ src/Protocols223/ParserProtocols223.java
@@ -1,14 +1,16 @@
 package Protocols223;
 
+import org.apache.commons.net.ftp.FTPFile;
+
 import java.sql.SQLException;
 import java.util.ArrayList;
 
 import static java.lang.System.out;
 
-public class ParserProtocols223 extends Parser{
+public class ParserProtocols223 extends Parser {
 
     @Override
-    public void Parsing(){
+    public void Parsing() {
         ArrayList<Region> reg = new ArrayList<>();
         try {
             reg = GetRegions();
@@ -17,8 +19,36 @@ public class ParserProtocols223 extends Parser{
         }
         for (Region r : reg) {
             //out.println(r.Name);
+            String pathParse = "";
+            for (String prot : protocols223Dir) {
+                switch (Main.arg) {
+                    case Last223:
+                        pathParse = String.format("/out/published/%s/%s/", r.Path223, prot);
+                        ParsingLast223(pathParse, prot, r);
+                        break;
+                    case Daily223:
+                        pathParse = String.format("/out/published/%s/%s/daily/", r.Path223, prot);
+                        break;
+                }
+            }
 
 
         }
+    }
+
+    public void ParsingLast223(String pathParse, String prot, Region region) {
+        GetListArchLast(pathParse, prot, region);
+
+    }
+
+    public ArrayList<FTPFile> GetListArchLast(String pathParse, String prot, Region region) {
+        ArrayList<FTPFile> arr = new ArrayList<>();
+        for (FTPFile ftpFile : arr = GetListFtp223(pathParse)) {
+            out.println(ftpFile.getName());
+        }
+
+
+        return arr;
+
     }
 }
